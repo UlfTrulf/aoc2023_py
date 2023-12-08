0161ac103e0a5e2b55bc443d9cbbc6c1905018f0
UlfTrulf <98895477+UlfTrulf@users.noreply.github.com>
2023-12-08 23:12:49 +0100

    add day08 add updated pythen venv

---
+++ py/day08.py
@@ -0,0 +1,56 @@
+import regex as re
+import math
+
+
+def reader(txt):
+    with open(txt) as file:
+        content = file.read().split('\n\n')
+    lines = content[1].split('\n')
+    instructions = []
+    for ins in lines:
+        instructions.append(re.split(r'=|,', ins.replace(' ', '')
+                            .replace('(', '')
+                            .replace(')', '')))
+    return content[0].replace('L', '1').replace('R', '2'), instructions
+
+
+def part1(txt):
+    return 0
+    current = 'AAA'
+    order, instructions = reader(txt)
+    i = 0
+    while current != 'ZZZ':
+        side = int(order[i % len(order)])
+        for x in instructions:
+            if x[0] == current:
+                current = x[side]
+                break
+
+        i += 1
+    return i
+
+
+def part2(txt):
+    order, instructions = reader(txt)
+    currents = []
+    for ins in instructions:
+        if ins[0][2] == 'A':
+            currents.append(ins[0])
+    loops = []
+    for current in currents:
+        c = current
+        i = 0
+        looped = False
+        visited = set()
+        while not looped:
+            side = int(order[i % len(order)])
+            for x in instructions:
+                if x[0] == c:
+                    visited.add((c, side))
+                    if (c, side) in visited and c[2] == 'Z':
+                        loops.append(i)
+                        looped = True
+                    c = x[side]
+                    break
+            i += 1
+    return math.lcm(*loops)
